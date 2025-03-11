a73d9b13b73483de465fcce18f89f62f2a9c4de0
joanitabexon <joanitabexon@gmail.com>
2025-03-11 18:17:53 +0000

    Add 7.py

---
+++ src/7.py
@@ -0,0 +1,11 @@
+def explore_science():
+    # Explore the wonders of science
+    print("I'm writing code in my project scientific-investigation (Exploring the wonders of science), please generate a random python code for me.")
+    import random
+    num = random.randint(1, 10)
+    if num == 3:
+        return "print('This is a cool feature!')"
+    elif num == 5:
+        return "import math; print(math.pi)"
+    else:
+        return "print('Hello World!')"
